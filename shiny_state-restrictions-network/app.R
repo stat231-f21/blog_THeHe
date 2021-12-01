2473edc24470eab547f29ad22a2ac0058afd2197
thuang2023 <89740668+thuang2023@users.noreply.github.com>
2021-12-01 11:39:55 -0500

    Add files via upload

---
+++ shiny_state-restrictions-network/app.R
@@ -0,0 +1,151 @@
+# Shiny app code for COVID state restrictions network
+# Tracy Huang
+
+# Load packages
+library(tidyverse)
+library(dplyr)
+library(igraph)
+library(visNetwork)
+library(grDevices)
+library(shiny)
+library(shinythemes)
+
+#######################
+#    Load Datasets    #
+#######################
+# Load network datasets
+restaurants_edges <- read.csv("restaurants_edges_data.csv") %>% select(-X)
+restaurants_nodes <- read.csv("restaurants_nodes_data.csv") %>% select(-X)
+
+bars_edges <- read.csv("bars_edges_data.csv") %>% select(-X)
+bars_nodes <- read.csv("bars_nodes_data.csv") %>% select(-X)
+
+mask_mandates_edges <- read.csv("mask_mandates_edges_data.csv") %>% select(-X)
+mask_mandates_nodes <- read.csv("mask_mandates_nodes_data.csv") %>% select(-X)
+
+gathering_ban_edges <- read.csv("gathering_ban_edges_data.csv") %>% select(-X)
+gathering_ban_nodes <- read.csv("gathering_ban_nodes_data.csv") %>% select(-X)
+
+stay_at_home_orders_edges <- read.csv("stay_at_home_orders_edges_data.csv") %>% select(-X)
+stay_at_home_orders_nodes <- read.csv("stay_at_home_orders_nodes_data.csv") %>% select(-X)
+
+# Load proportion of annual COVID cases per state population data
+annual_cases <- read.csv("proportion_annual_covid_cases_data.csv")
+
+##############################################
+# Define choice values and names for widgets #
+##############################################
+# For nodes
+node_values <- state.abb
+node_names <- state.name
+names(node_values) <- node_names
+
+# For datasets
+dataset_values <- c("rest", "bar", "mask", "gatban", "stayhome")
+dataset_names <- c("Restaurant Restrictions", "Bar Restrictions", "Mask Mandates", "Gathering Bans", "Stay at Home Orders")
+names(dataset_values) <- dataset_names
+
+############
+#    ui    #
+############
+ui <- fluidPage(
+  theme = shinytheme("lumen"),
+  title = "State COVID Restrictions Network",
+                fillPage(sidebarLayout(
+                  sidebarPanel(
+                    selectizeInput(
+                      inputId = "filterNodes",
+                      label = HTML("Select states <br />
+                      (To remove a state, select it and hit the delete button on your keyboard)"),
+                      choices = node_values,
+                      selected = "AK",
+                      multiple = TRUE
+                    ),
+                    selectInput(
+                      inputId = "restriction",
+                      label = "Select a type of COVID restriction",
+                      choices =  dataset_values,
+                      selected = "gatban",
+                      multiple = FALSE
+                    ),
+                    br(),
+                    p("Proportion of Annual COVID Cases per State Population"),
+                    img(src="covid_legend.png", width = "200px", height = "400px"),
+                    width = 3,
+                  ),
+                  mainPanel(
+                    visNetworkOutput("network_proxy_update", width = "100%", height = "90vh"),
+                    width = 9
+                  )
+                )))
+
+############
+# server   #
+############
+
+server <- function(input, output) {
+  
+  active_nodes <- reactive({
+    # Switch nodes dataset depending on user input
+    switch(input$restriction,
+           # subset each nodes dataset to only include the nodes the user selects
+           "rest" = subset(restaurants_nodes, restaurants_nodes$id %in% input$filterNodes), 
+           "bar" = subset(bars_nodes, bars_nodes$id %in% input$filterNodes),
+           "mask" = subset(mask_mandates_nodes, mask_mandates_nodes$id %in% input$filterNodes),
+           "gatban" = subset(gathering_ban_nodes, gathering_ban_nodes$id %in% input$filterNodes),
+           "stayhome" = subset(stay_at_home_orders_nodes, stay_at_home_orders_nodes$id %in% input$filterNodes)
+    )
+  })
+  
+  active_edges <- reactive({
+    # Switch edges dataset depending on user input
+    switch(input$restriction,
+           # subset each edges dataset to only include the nodes the user selects
+           "rest" = subset(restaurants_edges, restaurants_edges$from %in% input$filterNodes),
+           "bar" = subset(bars_edges, bars_edges$from %in% input$filterNodes),
+           "mask" = subset(mask_mandates_edges, mask_mandates_edges$from %in% input$filterNodes),
+           "gatban" = subset(gathering_ban_edges, gathering_ban_edges$from %in% input$filterNodes),
+           "stayhome" = subset(stay_at_home_orders_edges, stay_at_home_orders_edges$from %in% input$filterNodes)
+    )
+  })
+  
+  output$network_proxy_update <- renderVisNetwork({
+    visNetwork(active_nodes(), active_edges(), height = "700px", width = "100%", 
+               # Create a title and subtitle for network
+               main = list(text = paste("Network for States Sharing the Same COVID", dataset_names[dataset_values == input$restriction], "in 2020"), 
+                           style = "font-family:Arial;font-size:20px"),
+               submain = list(text = "*Nodes are colored by proportion of annual COVID cases per state population<br>
+                                 *Edges are weighted by number of days in 2020 that states shared 
+                                 the same restriction order<br>
+                                 Hover over edges to see the exact number of days",
+                           style = "font-family:Arial;font-size:13px")) %>%
+      # Specify size of nodes
+      visNodes(size = 10) %>%
+      # Allow users to select a node and have its edges highlighted
+      visOptions(selectedBy = "group", 
+                 highlightNearest = list(enabled = TRUE, hover = TRUE), 
+                 nodesIdSelection = TRUE) %>%
+      visPhysics(stabilization = FALSE) %>%
+      # Specify color of edges
+      visEdges(color = list(color = "black", highlight = "red")) 
+  })
+  
+  # Create an updated network object
+  myVisNetworkProxy <- visNetworkProxy("network_proxy_update")
+  
+  observe ({
+    # Create a data frame to contain the nodes the user selects
+    filteredNodes <- active_nodes()[gathering_ban_nodes$id %in% input$filterNodes, ,drop = FALSE]
+    # Create a data frame to contain all the nodes the user did not select
+    hiddenNodes <- anti_join(active_nodes(), filteredNodes)
+    # Remove nodes that user did not select
+    visRemoveNodes(myVisNetworkProxy, id = hiddenNodes$id)
+    # Add nodes that user selects
+    visUpdateNodes(myVisNetworkProxy, nodes = filteredNodes)
+  })
+}
+
+#####################
+# call to shiny App #
+#####################
+shinyApp(ui = ui, server = server)
